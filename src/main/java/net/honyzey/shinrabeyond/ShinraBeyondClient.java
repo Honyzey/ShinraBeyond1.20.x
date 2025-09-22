a32431a9c1fc9ca807a5879c3795990a7dfc50dc
Honyzey <briac35.v@gmail.com>
2025-09-22 16:43:44 +0200

    initial commit

---
+++ src/main/java/net/honyzey/shinrabeyond/ShinraBeyondClient.java
@@ -0,0 +1,10 @@
+package net.honyzey.shinrabeyond;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShinraBeyondClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/honyzey/shinrabeyond/ShinraBeyondDataGenerator.java
@@ -0,0 +1,11 @@
+package net.honyzey.shinrabeyond;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ShinraBeyondDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
